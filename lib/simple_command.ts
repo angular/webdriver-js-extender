e20754f8d598ab83a42b59f9908d7852aa999c5e
Sammy Jelin <sjelin@gmail.com>
2016-12-06 14:37:54 -0800

    feat(extender): Allow `fallbackOnWD` option
    When `true`, this means that if the WebDriver instance cannot be extended, the extend()
    function will simply return the origonal WebDriver instance instead of throwing an error

--- lib/simple_command.ts
+++ lib/simple_command.ts
@@ -7,11 +7,23 @@ export class SimpleCommand<T> {
       private name: string, private params: string[], private method: string,
       private path: string) {}
 
-  compile(extender: Extender) {
+  compile(extender: Extender, silentFailure: boolean) {
     let name = this.name;
-    extender.defineCommand(name, this.params, this.method, this.path);
-    return function(...args: any[]): Promise.Promise<T> {
-      return extender.execCommand<T>(name, args);
-    };
+    try {
+      extender.defineCommand(name, this.params, this.method, this.path);
+      return function(...args: any[]): Promise.Promise<T> {
+        return extender.execCommand<T>(name, args);
+      };
+    } catch (e) {
+      if (silentFailure) {
+        return function(...args: any[]) {
+          throw new Error(
+              'Command "' + name + '" could not be extended onto WebDriver instance. ' +
+              'This is generally a result of using `directConnect` in protractor.');
+        };
+      } else {
+        throw e;
+      }
+    }
   }
 }
