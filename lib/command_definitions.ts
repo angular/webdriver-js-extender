088d5dfbe9044a3be46d57d69ea72c3b15b02aa4
Sammy Jelin <sjelin@gmail.com>
2016-12-27 14:40:16 -0800

    feat(commands): more commands (#10)
    All the ones not implemented in https://github.com/angular/webdriver-js-extender/pull/9.

--- lib/command_definitions.ts
+++ lib/command_definitions.ts
@@ -4,3 +4,58 @@ export let getNetworkConnection =
     new CommandDefinition<number>('getNetworkConnection', [], 'GET', '/network_connection');
 export let setNetworkConnection =
     new CommandDefinition<void>('setNetworkConnection', ['type'], 'POST', '/network_connection');
+
+export let listContexts = new CommandDefinition<string[]>('listContexts', [], 'GET', '/contexts');
+export let uploadFile = new CommandDefinition<void>('uploadFile', ['file'], 'POST', '/file');
+export let switchToParentFrame =
+    new CommandDefinition<void>('switchToParentFrame', [], 'POST', '/frame/parent');
+export let fullscreen = new CommandDefinition<void>('fullscreen', [], 'POST', '/window/fullscreen');
+export let sendAppToBackground = new CommandDefinition<void>(
+    'sendAppToBackground', ['seconds'], 'POST', '/appium/app/background', (args: [number]) => {
+      args[0] = args[0] || 0;
+      return args;
+    });
+export let closeApp = new CommandDefinition<void>('closeApp', [], 'POST', '/appium/app/close');
+export let getAppStrings = new CommandDefinition<string[]>(
+    'getAppStrings', ['language'], 'POST', 'appium/app/strings', (args: string[]) => {
+      args[0] = args.length ? args[0] : undefined; // Default to `undefined`
+      return args;
+    });
+export let launchSession =
+    new CommandDefinition<void>('launchSession', [], 'POST', '/appium/app/launch');
+export let resetApp = new CommandDefinition<void>('resetApp', [], 'POST', '/appium/app/reset');
+export let hideSoftKeyboard = new CommandDefinition<void>(
+    'hideSoftKeyboard', ['strategy', 'key'], 'POST', '/appium/device/hide_keyboard',
+    (args: string[]) => {
+      switch (args[0] || 'default') {
+        case 'default':
+          args[0] = 'default';
+        case 'swipeDown':
+        case 'tapOut':
+        case 'tapOutside':
+          if (args.length == 1) {
+            args[1] = undefined; // Default to `undefined`
+          }
+        case 'press':
+        case 'pressKey':
+          return args;
+        default:
+          throw new RangeError('Invalid keyboard hiding strategy "' + args[0] + '"');
+      }
+    });
+export let getDeviceTime =
+    new CommandDefinition<string>('getDeviceTime', [], 'GET', '/appium/device/system_time');
+export let openDeviceNotifications = new CommandDefinition<void>(
+    'openDeviceNotifications', [], 'POST', '/appium/device/open_notifications');
+export let rotationGesture = new CommandDefinition(
+    'rotationGesture', ['x', 'y', 'duration', 'rotation', 'touchCount'], 'POST',
+    '/appium/device/rotate', (args: number[]) => {
+      args[0] = args[0] || 0;
+      args[1] = args[1] || 0;
+      args[2] = args[2] === undefined ? 1 : args[2];
+      args[3] = args[3] === undefined ? 180 : args[3];
+      args[4] = args[4] == undefined ? 2 : args[4];
+      return args;
+    });
+export let shakeDevice =
+    new CommandDefinition<void>('shakeDevice', [], 'POST', 'appium/device/shake');
--- spec/command_tests/table_spec.ts
+++ spec/command_tests/table_spec.ts
@@ -15,8 +15,8 @@ describe('table tests', () => {
   let table: {[commandName: string]: Testcase | Testcase[]} = {
     getCurrentContext: {skip: true, result: 'WEBVIEW_1'},
     selectContext: {skip: true, args: ['WEBVIEW_1'], params: {name: 'WEBVIEW_1'}},
-    listContexts: {skip: true, result: ['WEBVIEW_1']},
-    uploadFile: {skip: true, args: ['hello'], params: {file: 'hello'}},
+    listContexts: {result: ['WEBVIEW_1']},
+    uploadFile: {args: ['hello'], params: {file: 'hello'}},
     getNetworkConnection: {result: 6},
     setNetworkConnection: {args: [0], params: {'type': 0}},
     toggleAirplaneMode: {skip: true},
@@ -28,24 +28,24 @@ describe('table tests', () => {
         {latitude: 1, longitude: 2, altitude: 3}}},
     getScreenOrientation: {skip: true, result: 'PORTRAIT'},
     setScreenOrientation: {skip: true, args: ['landscape'], params: {orientation: 'LANDSCAPE'}},
-    switchToParentFrame: {skip: true},
-    fullscreen: {skip: true},
+    switchToParentFrame: {},
+    fullscreen: {},
 
     getAppiumSettings: {skip: true, result: {ignoreUnimportantViews: false}},
     setAppiumSettings: {skip: true, args: [{ignoreUnimportantViews: true}], params: {settings: 
         {ignoreUnimportantViews: true}}},
-    sendAppToBackground: [{skip: true, params: {seconds: 0}},
-        {skip: true, args: [1], params: {seconds: 1}}],
-    closeApp: {skip: true},
-    getAppStrings: {skip: true, result: ['Hello', 'World']},
-    launchSession: {skip: true},
-    resetApp: {skip: true},
+    sendAppToBackground: [{params: {seconds: 0}}, {args: [1], params: {seconds: 1}}],
+    closeApp: {},
+    getAppStrings: [{result: ['Hello', 'World']},
+        {result: ['Hello', 'World'], args: ['en'], params: {language: 'en'}}],
+    launchSession: {},
+    resetApp: {},
     getCurrentDeviceActivity: {skip: true},
     startDeviceActivity: [{skip: true, args: ['a', 'b', 'c', 'd'], params:
         {appPackage: 'a', appActivity: 'b', appWaitPackage: 'c', appWaitActivity: 'd'}},
         {skip: true, args: ['a', 'b'], params: {appPackage: 'a', appActivity: 'b'}}],
-    hideSoftKeyboard: [{skip: true, params: {strategy: 'default'}},
-        {skip: true, args: ['pressKey', 'Done'], params: {strategy: 'pressKey', key: 'Done'}}],
+    hideSoftKeyboard: [{params: {strategy: 'default'}},
+        {args: ['pressKey', 'Done'], params: {strategy: 'pressKey', key: 'Done'}}],
     installApp: {skip: true, args: [path.resolve(__dirname, 'totally_real_apk.apk')],
         params: {appPath: path.resolve(__dirname, 'totally_real_apk.apk')}},
     isAppInstalled: {skip: true, result: false, args: ['sjelin.is.cool'], params: {bundleId: 'sjelin.is.cool'}},
@@ -56,12 +56,11 @@ describe('table tests', () => {
 //    pullFileFromDevice: null,  // No good way to test this
     pullFolderFromDevice: {skip: true, result: {}, args: [''], params: {path: ''}},
     pushFileToDevice: {skip: true, args: ['/a/b', 'cde'], params: {path: '/a/b', data: 'cde'}},
-    getDeviceTime: {skip: true},
-    openDeviceNotifications: {skip: true},
-    rotationGesture: [{skip: true, params: {x: 0, y: 0, duration: 1, rotation: 180,
-        touchCount: 2}}, {skip: true, args: [1,2,3,90,5], params:
-        {x: 1, y: 2, duration: 3, rotation: 90, touchCount: 5}}],
-    shakeDevice: {skip: true}
+    getDeviceTime: {result: new Date().toString()},
+    openDeviceNotifications: {},
+    rotationGesture: [{params: {x: 0, y: 0, duration: 1, rotation: 180, touchCount: 2}},
+        {args: [1,2,3,90,5], params: {x: 1, y: 2, duration: 3, rotation: 90, touchCount: 5}}],
+    shakeDevice: {}
   }
   function runTestcase(commandName: string) {
     let itName = 'should correctly call "' + commandName + '"';
